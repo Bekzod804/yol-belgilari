166637d012436da7b98889febf6ae956434b40c0
Bekzod804 <raxmonovb215@gmail.com>
2021-02-09 17:00:35 +0500

    first commit

---
+++ js/array.js
@@ -0,0 +1,534 @@
+var signsArray = [{
+
+    title: "Yo'l bering",
+    img: "./images/signs/yol_bering.gif"
+
+  },
+  {
+
+    title: "Turar-joy dahasi",
+    img: "./images/signs/aholi_punkti.gif"
+  },
+  {
+
+    title: "Asosiy yolning oxiri",
+    img: "./images/signs/asosiy_tugash.gif"
+  },
+  {
+
+    title: "Asosiy yo’lning yo’nalishi",
+    img: "./images/signs/asosiy_yol_sxemasi.gif"
+  },
+  {
+
+    title: "Asosiy yol",
+    img: "./images/signs/asosiy_yol.gif"
+  },
+  {
+
+    title: "Avtomagistral",
+    img: "./images/signs/avtomagistral.gif"
+  },
+  {
+
+    title: "Avtomagistral oxiri",
+    img: "./images/signs/avtomagistral_tugashi.gif"
+  },
+  {
+
+    title: "Velosiped yolkasi",
+    img: "./images/signs/velosiped_yolkasi.gif"
+  },
+  {
+
+    title: "Transport vositalarini yuvish joyi",
+    img: "./images/signs/avtomoyka.gif"
+  },
+  {
+
+    title: "Texnik xizmat ko’rsatish joyi",
+    img: "./images/signs/avtoservis.gif"
+  },
+  {
+
+    title: "Aylanma harakatlanish",
+    img: "./images/signs/aylanma_yol.gif"
+  },
+  {
+
+    title: "Belgilangan yo’nalishli transport vositalari uchun mo’ljallangan bo’lak",
+    img: "./images/signs/belgilangan_yonalishli_transport_yolak.gif"
+  },
+  {
+
+    title: "Yonilg’i shaxobchasi",
+    img: "./images/signs/benzin.gif"
+  },
+  {
+
+    title: "Bir izli temir yo’l",
+    img: "./images/signs/bir_izli_temir.gif"
+  },
+  {
+
+    title: "Bolalar",
+    img: "./images/signs/bolalar.gif"
+  },
+  {
+
+    title: "Oxiri berk yo’l, ko’cha",
+    img: "./images/signs/boshi_berk_kocha.gif"
+  },
+  {
+
+    title: "Boshqa xavf-xatar",
+    img: "./images/signs/boshqa_xavf.gif"
+  },
+  {
+
+    title: "Chapga burilish taqiqlanadi",
+    img: "./images/signs/burilish_taqiqlanadi.gif"
+  },
+  {
+
+    title: "Eng kam tezlik belgilangan yo’lning oxiri",
+    img: "./images/signs/eng_kam_tezlik_oxiri.gif"
+  },
+  {
+
+    title: "Avtomobillarni ko’rikdan o’tkazish joyi",
+    img: "./images/signs/estakada.gif"
+  },
+  {
+
+    title: "Harakatlanish taqiqlangan",
+    img: "./images/signs/faqat_odamlaga.gif"
+  },
+  {
+
+    title: "Harakatlanish chapga",
+    img: "./images/signs/harakatlanish_chapga.gif"
+  },
+  {
+
+    title: "Harakatlanish o'nga",
+    img: "./images/signs/harakatlanish_onga.gif"
+  },
+  {
+
+    title: "Harakatlanish o'nga yoki tog'riga",
+    img: "./images/signs/harakatlanish_onga_togriga.gif"
+  },
+  {
+
+    title: "Harakatlanish tasviri",
+    img: "./images/signs/harakatlanish_tasviri.gif"
+  },
+  {
+
+    title: "Harakatlanish tog'riga",
+    img: "./images/signs/harakatlanish_togriga.gif"
+  },
+  {
+
+    title: "Ichimlik suvi",
+    img: "./images/signs/ichimlik_suvi.gif"
+  },
+  {
+
+    title: "Ikki tomonlama harakatlanish",
+    img: "./images/signs/ikki_tomonlama_xarakatlanish.gif"
+  },
+  {
+
+    title: "Dam olish joyi",
+    img: "./images/signs/kemping.gif"
+  },
+  {
+
+    title: "Kirish taqiqlangan",
+    img: "./images/signs/kirish_taqiqlanadi.gif"
+  },
+  {
+
+    title: "Mehmonxona",
+    img: "./images/signs/kasalxona.gif"
+  },
+  {
+
+    title: "Kotarma koprik",
+    img: "./images/signs/kotarma_koprik.gif"
+  },
+  {
+
+    title: "Cheklangan balandlik",
+    img: "./images/signs/maksimal_balandlik.gif"
+  },
+  {
+
+    title: "Cheklangan kenglik",
+    img: "./images/signs/maksimal_kenglik.gif"
+  },
+  {
+
+    title: "Vazn cheklangan",
+    img: "./images/signs/maksimal_ogrlik.gif"
+  },
+  {
+
+    title: "Cheklangan uzunlik",
+    img: "./images/signs/maksimal_uzunlik.gif"
+  },
+  {
+
+    title: "Avtobus va trolleybus to’xtash joyi",
+    img: "./images/signs/marshrut.gif"
+  },
+  {
+
+    title: "Mexanik transport vositalarining harakatlanishi taqiqlangan",
+    img: "./images/signs/mexanik_taqiqlangan.gif"
+  },
+  {
+
+    title: "Mototsikllar harakatlanishi taqiqlangan",
+    img: "./images/signs/motosikl_taqiqlanadi.gif"
+  },
+  {
+
+    title: "Eng kam oraliq",
+    img: "./images/signs/oraliq_masofa.gif"
+  },
+  {
+
+    title: "Oshxona",
+    img: "./images/signs/oshxona.gif"
+  },
+  {
+
+    title: "Ot-arava harakatlanishi taqiqlangan",
+    img: "./images/signs/ot_arava_taqiqlanadi.gif"
+  },
+  {
+
+    title: "To’xtab turish joyi",
+    img: "./images/signs/parkovka.gif"
+  },
+  {
+
+    title: "Pastlab uchuvchi samolyotlar",
+    img: "./images/signs/pastlab_uchuvchi_samolyot.gif"
+  },
+  {
+
+    title: "Piyodalar o’tish joyi",
+    img: "./images/signs/piyodalar_otish_joyi.gif"
+  },
+  {
+
+    title: "Piyodalar yo’lkasi",
+    img: "./images/signs/piyodalar_yolagi.gif"
+  },
+  {
+   
+    title: "Shlagbaumsiz temir yo’l kesishmasi",
+    img: "./images/signs/poyezd.gif"
+  },
+  {
+
+    title: "Qayrilish ta'qiqlanadi",
+    img: "./images/signs/qayrilish_taqiqlanadi.gif"
+  },
+  {
+    title: "Boshqa qatnov qismiga qayta tizilishning boshlang’ich ko’rsatkichi",
+    img: "./images/signs/qayta_tizilish_sxemasi.gif"
+  },
+  {
+    title: "Quvib o’tish taqiqlanadi",
+    img: "./images/signs/quvib_otish_taqiqlanadi.gif"
+  },
+  {
+
+    title: "Quvib o’tish ta'qiqlangan hududning oxiri",
+    img: "./images/signs/quvib_otish_taqiqlanadi_tugadi.gif"
+  },
+  {
+
+    title: "Radar",
+    img: "./images/signs/radar.jpg"
+  },
+  {
+
+    title: "Ro’paradagi harakatlanishga nisbatan imtiyoz",
+    img: "./images/signs/ropara_imtiyo.gif"
+  },
+  {
+
+    title: "Ro’para harakatlanishning ustunligi",
+    img: "./images/signs/ropara_ustunlik.gif"
+  },
+  {
+
+    title: "Shlagbaumli temir yo'l kesishmasi",
+    img: "./images/signs/shlakbaumli_temiryol.gif"
+  },
+  {
+
+    title: "Sirpanchiq yo’l",
+    img: "./images/signs/sirpanchiq_yol.gif"
+  },
+  {
+
+    title: "Sohilga chiqish",
+    img: "./images/signs/soxilga_chiqish.gif"
+  },
+  {
+
+    title: "To’xtamasdan harakatlanish taqiqlangan",
+    img: "./images/signs/stop.gif"
+  },
+  {
+
+    title: "Svetofor tartibga soladi",
+    img: "./images/signs/svetofor.gif"
+  },
+  {
+
+    title: "Ta’mirlash ishlari",
+    img: "./images/signs/tamirlash_ishlari.gif"
+  },
+  {
+
+    title: "Ta’sir oralig’i",
+    img: "./images/signs/tasir_doirasi.gif"
+  },
+  {
+
+    title: "Taksi to’xtab turish joyi",
+    img: "./images/signs/taxi.gif"
+  },
+  {
+
+    title: "Telefon",
+    img: "./images/signs/telefon.gif"
+  },
+  {
+
+    title: "Temir yo’l kesishmasining yaqinligi haqida ogohlantirish",
+    img: "./images/signs/temiryol_kesishmasining_yaqinligi.gif"
+  },
+  {
+
+    title: "Yuqori tezlik cheklangan",
+    img: "./images/signs/tezlik-cheklangan.gif"
+  },
+  {
+
+    title: "Tik nishablik",
+    img: "./images/signs/tik_nishablik.gif"
+  },
+  {
+
+    title: "Tik balandlik",
+    img: "./images/signs/tik_qiyalik.gif"
+  },
+  {
+
+    title: "Tirkama bilan harakatlanish taqiqlangan",
+    img: "./images/signs/tirkama_taqiq.gif"
+  },
+  {
+
+    title: "Xojatxona",
+    img: "./images/signs/toilet.gif"
+  },
+  {
+
+    title: "Tonnel",
+    img: "./images/signs/tonnel.gif"
+  },
+  {
+
+    title: "Tosh otilishi",
+    img: "./images/signs/tosh_otilishi.gif"
+  },
+  {
+    title: "Toshlar tushishi",
+    img: "./images/signs/tosh_tushishi.gif"
+  },
+  {
+    title: "To’siqni o’ngdan yoki chapdan chetlab o’tish",
+    img: "./images/signs/tosiq_chap_ili_ong.gif"
+  },
+  {
+    title: "To’siqni chapdan chetlab o’tish",
+    img: "./images/signs/tosiq_chapdan.gif"
+  },
+  {
+    title: "To’siqni chapdan o'ngdan chetlab o'tish",
+    img: "./images/signs/tosiq_ongdan.gif"
+  },
+  {
+    title: "Tovush moslamalaridan foydalanish ta'qiqlangan",
+    img: "./images/signs/tovushli_signal.gif"
+  },
+  {
+    title: "To’xtab turish ta'qiqlangan",
+    img: "./images/signs/toxtab_turish.gif"
+  },
+  {
+
+    title: "To’xtash taqiqlangan",
+    img: "./images/signs/toxtash_taqiqlanadi.gif"
+  },
+  {
+
+    title: "Transport vositasini to’xtab turish joyiga qo’yish usuli",
+    img: "./images/signs/toxtash_usuli.gif"
+  },
+  {
+
+    title: "Traktorlar harakatlanishi taqiqlangan",
+    img: "./images/signs/traktor.gif"
+  },
+  {
+
+    title: "Tramvay yo’li bilan kesishuv",
+    img: "./images/signs/tramvay_kesishuv.gif"
+  },
+  {
+
+    title: "Tramvay to’xtash joyi",
+    img: "./images/signs/tramvay.gif"
+  },
+  {
+
+    title: "Transport vositasining turi",
+    img: "./images/signs/transport_vositasi_turi.gif"
+  },
+  {
+
+    title: "Turar-joy dahasining oxiri",
+    img: "./images/signs/turar_joy_tugash.gif"
+  },
+  {
+
+    title: "Mol haydab o’tish",
+    img: "./images/signs/uy_hayvonlari.gif"
+  },
+  {
+
+    title: "Velosipedda harakatlanish ta'qiqlangan",
+    img: "./images/signs/velik_taqiqlanadi.gif"
+  },
+  {
+
+    title: "Velosiped yo’lkasi bilan kesishuv",
+    img: "./images/signs/velosiped.gif"
+  },
+  {
+
+    title: "Piyodalarning yer ostidan o’tish joyi",
+    img: "./images/signs/yer_osti_yoli.gif"
+  },
+  {
+
+    title: "Piyodalarning yer ustidan o’tish joyi",
+    img: "./images/signs/yer_usti_yoli.gif"
+  },
+  {
+
+    title: "Yonlama shamol",
+    img: "./images/signs/yonlama_shamol.gif"
+  },
+  {
+
+    title: "Yovvoyi hayvonlar",
+    img: "./images/signs/yovvoyi_hayvonlar.gif"
+  },
+  {
+
+    title: "Yuk avtomobillarida quvib o’tish taqiqlangan",
+    img: "./images/signs/yuk_avtomobil_quvish_taqiqlanadi.gif"
+  },
+  {
+
+    title: "Yuk avtommobillarida quvib o’tish taqiqlangan hududning oxiri",
+    img: "./images/signs/yuk_avtomobil_quvish_taqiqlanadi_oxiri.gif"
+  },
+  {
+
+    title: "Tirbandlik",
+    img: "./images/signs/tirbandlik.gif"
+  },
+  {
+
+    title: "Notekis yo’l",
+    img: "./images/signs/notekis_yol.gif"
+  },
+  {
+
+    title: "Barcha cheklovlarning oxiri",
+    img: "./images/signs/barcha.gif"
+  },
+  {
+
+    title: "Tavsiya etilgan tezlik",
+    img: "./images/signs/tavsiya_etilgan_tezlik.gif"
+  },
+  {
+
+    title: "Surat va video",
+    img: "./images/signs/surat_foto.png"
+  },
+  {
+    title: "Falokatli holatlar uchun kirish yo’li",
+    img: "./images/signs/xatar_yol.gif"
+  },
+  {
+
+    title: "Shifoxona",
+    img: "./images/signs/shifoxona.gif"
+  },
+  {
+
+    title: "Yuqori tezlik cheklangan hududning oxiri",
+    img: "./images/signs/maksimal_tezlik_tugadi.gif"
+  },
+];
+// var yangiArray = [];
+
+// var newTemplate = function () {
+//   signsArray.forEach(belgi => {
+//     var newTemplateElement = templateElement.cloneNode(true);
+//     newTemplateElement.querySelector(".img").src = belgi.img;
+//     newTemplateElement.querySelector(".title").textContent = belgi.title;
+//     console.log(console.log(newTemplateElement.querySelector(".img").src = belgi.img));
+//     console.log(newTemplateElement.querySelector(".title").textContent = belgi.title);
+//     yangiArray.push(newTemplateElement);
+
+// });
+// }
+// function creating() {
+//   for (let index = 0; index < signsArray.length; index++) {
+//     var newTemplateElement = templateElement.cloneNode(true);
+//     newTemplateElement.querySelector(".img").src = belgi.img;
+//     newTemplateElement.querySelector(".title").textContent = belgi.title;
+//     console.log(console.log(newTemplateElement.querySelector(".img").src = belgi.img));
+//     console.log(newTemplateElement.querySelector(".title").textContent = belgi.title);
+//      templateDiv.appendChild(newTemplateElement)
+
+//   }
+// }
+// newTemplate()
+// console.log(yangiArray)
+// var executing = (items => {
+//   templateDiv.innerHTML = "";
+//   var bekzod = document.createDocumentFragment();
+//  console.log(bekzod)
+//   items.forEach(item => {
+//    bekzod.appendChild(newTemplateElement);
+//    console.log(bekzod)
+//   })
+//    console.log(bekzod)
+// })
---
+++ js/main.js
@@ -0,0 +1,252 @@
+var blockElement = document.querySelector(".block");
+var signsDiv = document.querySelector(".signs");
+blockElement.addEventListener("click", function (evt) {
+  var first = blockElement.firstElementChild;
+  var last = blockElement.lastElementChild;
+  evt.preventDefault();
+  if (evt.target.matches(".block__link")) {
+    evt.target.classList.add("class");
+    if (evt.target == first) {
+      evt.target.nextElementSibling.classList.remove("class");
+      last.classList.remove("class")
+    }
+    if (evt.target == last) {
+      evt.target.previousElementSibling.classList.remove("class");
+      first.classList.remove("class")
+    } else {
+      evt.target.previousElementSibling.classList.remove("class");
+      evt.target.nextElementSibling.classList.remove("class");
+    }
+  }
+})
+var templateDiv = document.querySelector(".template-div");
+var templateElement = document.querySelector(".template").content;
+var bek = signsArray.slice(0, 20);
+var bek1 = signsArray.slice(0, 40);
+var bek2 = signsArray.slice(0, 80);
+var levelSelect = document.querySelector(".level-select");
+var timeSelect = document.querySelector(".time-select");
+var btnStart = document.querySelector(".btn-outline");
+var salom;
+var salom1;
+var salom2;
+var alik;
+var alik1;
+var alik2;
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+var createInput = document.createElement("input");
+levelSelect.addEventListener("change", function () {
+  excuting2()
+  if (this.value == "oson") {
+
+    function creating(array) {
+      templateDiv.innerHTML = "";
+      createInput.type = "text";
+      createInput.classList = "form-control";
+      createInput.style.marginBottom = "20px";
+      createInput.style.padding = "10px";
+      createInput.style.fontSize = "30px";
+
+      templateDiv.appendChild(createInput);
+      for (let index = 0; index < 20; index++) {
+        var newTemplateElement = templateElement.cloneNode(true);
+        newTemplateElement.querySelector(".img").src = array[index].img;
+        newTemplateElement.querySelector(".title").textContent = array[index].title;
+        templateDiv.appendChild(newTemplateElement)
+      }
+      setInterval(() => {
+        salom = getRandomInt(bek.length);
+        alik = bek[salom];
+        createInput.value = bek[salom].title;
+        createInput.style.textAlign = "center"
+      }, 6000);
+    }
+    creating(signsArray)
+  }
+  if (this.value == "ortacha") {
+
+    function creating(array) {
+      templateDiv.innerHTML = "";
+      createInput.type = "text";
+      createInput.classList = "form-control";
+      createInput.style.marginBottom = "20px";
+      createInput.style.padding = "10px";
+      createInput.style.fontSize = "30px";
+      templateDiv.appendChild(createInput);
+      for (let index = 0; index < 40; index++) {
+        var newTemplateElement = templateElement.cloneNode(true);
+        newTemplateElement.querySelector(".img").src = array[index].img;
+        newTemplateElement.querySelector(".title").textContent = array[index].title;
+        templateDiv.appendChild(newTemplateElement)
+
+      }
+      setInterval(() => {
+        salom1 = getRandomInt(bek1.length);
+        alik = bek1[salom1];
+        createInput.value = bek[salom1].title;
+        createInput.style.textAlign = "center"
+      }, 6000);
+    }
+    creating(signsArray)
+  }
+  if (this.value == "qiyin") {
+
+    function creating(array) {
+      templateDiv.innerHTML = "";
+      createInput.type = "text";
+      createInput.classList = "form-control";
+      createInput.style.marginBottom = "20px";
+      createInput.style.padding = "10px";
+      createInput.style.fontSize = "30px";
+      templateDiv.appendChild(createInput);
+      for (let index = 0; index < signsArray.length; index++) {
+        var newTemplateElement = templateElement.cloneNode(true);
+        newTemplateElement.querySelector(".img").src = array[index].img;
+        newTemplateElement.querySelector(".title").textContent = array[index].title;
+        templateDiv.appendChild(newTemplateElement)
+      }
+      setInterval(() => {
+        salom2 = getRandomInt(bek2.length);
+        alik2 = bek[salom2];
+        createInput.value = bek2[salom2].title;
+        createInput.style.textAlign = "center"
+      }, 6000);
+    }
+    creating(signsArray)
+  }
+})
+var creteDiv = document.createElement("div");
+var closeBtn = document.querySelector(".exit");
+
+templateDiv.addEventListener("click", function (evt) {
+  if (evt.target.matches(".div")) {
+    if (evt.target.firstElementChild.textContent == createInput.value) {
+      evt.target.classList.add("salom");
+      evt.target.style.visibility = "hidden"
+      var some = bek.findIndex(item => item == alik);
+      var bekzod = bek.splice(some1, 1);
+      var some1 = bek1.findIndex(item => item == alik1);
+      var bekzod1 = bek1.splice(some1, 1);
+    }
+    if (evt.target.firstElementChild.textContent != createInput.value) {
+      evt.target.classList.add("border")
+    }
+  }
+})
+
+var body = document.querySelector("body");
+var matn = document.createElement("div");
+var div = document.querySelectorAll(".div")
+var timeSelect = document.querySelector(".time-select");
+btnStart.addEventListener("click", function (evt) {
+  body.style.backgroundImage = "none";
+  document.querySelector(".select-wrapper").remove();
+  var id = 0;
+  matn.classList = "btn btn-danger"
+  matn.style.color = "#ffffff";
+  matn.style.fontSize = "25px";
+  matn.style.boxShadow = " 0 0 20px #000000"
+  matn.style.padding = "11px 38px";
+  matn.style.position = "fixed";
+  matn.style.top = "-102px";
+  matn.style.right = "20px";
+  closeBtn.style.left = "20px";
+  matn.style.borderRadius = "5px"
+  body.appendChild(matn);
+  setInterval(() => {
+    matn.textContent = id++
+  }, 1000);
+
+
+  templateDiv.classList.add("kim1");
+  evt.preventDefault();
+  var form = document.querySelector(".form");
+  form.style.height = "0";
+  var formElement = form.querySelector(".form__div");
+  formElement.style.height = "0";
+  creteDiv.classList.add("new");
+  var TimeInterval = function (salom) {
+    var TimeInterval = setInterval((salom) => {
+        var tugma = document.createElement("button");
+        tugma.classList = "btn ";
+        tugma.style.backgroundColor = "rgb(19, 196, 3)";
+        tugma.style.boxShadow = " 0 0 20px rgb(6, 59, 1)";
+        tugma.style.fontSize = "25px"
+        tugma.textContent = "close";
+        tugma.style.position = "fixed";
+        tugma.style.top = "77%";
+        tugma.style.zIndex = "1000"
+        tugma.style.left = "47%";
+        tugma.style.padding = "10px 20px"
+        body.innerHTML = "";
+        tugma.addEventListener("click", function () {
+          window.location.href = "./index.html";
+        })
+        body.append(creteDiv, tugma);
+        if (bek.length >= -1) {
+          creteDiv.textContent = "Uzr siz keyingi bosqichga ota olmadingiz.Siz oyinni yangidan boshlashingiz mumkin.Buning uchun close tugmasini bosing";
+
+        } else {
+          creteDiv.textContent = "Tabriklaymiz siz golib boldingiz.Siz exit tugmasini bosish orqali orqaga qaytib qiyinroq bosqichni tanlashingiz mumkin";
+        }
+        if (bek1.length >= -1) {
+          creteDiv.textContent = "Uzr siz keyingi bosqichga ota olmadingiz.Siz oyinni yangidan boshlashingiz mumkin.Buning uchun close tugmasini bosing";
+
+        } else {
+          creteDiv.textContent = "Tabriklaymiz siz golib boldingiz.Siz exit tugmasini bosish orqali orqaga qaytib qiyinroq bosqichni tanlashingiz mumkin";
+        }
+        if (bek2.length >= -1) {
+          creteDiv.textContent = "Uzr siz keyingi bosqichga ota olmadingiz.Siz oyinni yangidan boshlashingiz mumkin.Buning uchun close tugmasini bosing";
+
+        } else {
+          creteDiv.textContent = "Tabriklaymiz siz golib boldingiz.Siz exit tugmasini bosish orqali orqaga qaytib qiyinroq bosqichni tanlashingiz mumkin";
+        }
+
+      },
+
+      salom);
+  }
+  var calling = function () {
+    if (timeSelect.value == "ikki") {
+      TimeInterval(100000)
+    }
+
+  }
+  calling();
+  var calling1 = function () {
+    if (timeSelect.value == "uch") {
+      TimeInterval(200000);
+    }
+
+  }
+  calling1();
+  var calling2 = function () {
+    if (timeSelect.value == "besh") {
+      TimeInterval(600000)
+    }
+
+  }
+  calling2();
+  form.remove();
+  closeBtn.addEventListener("click", function () {
+    clearInterval(TimeInterval);
+    creteDiv.remove();
+  })
+})
+
+closeBtn.addEventListener("click", function (evt) {
+  window.location.href = "./index.html";
+})
+
+function excuting2() {
+  if (levelSelect.value === 'bek') {
+    btnStart.disabled = true;
+  } else {
+    btnStart.disabled = false;
+  }
+}
+excuting2()
